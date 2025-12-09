33849882a0803a990f54206fac8461b7d255e7a1
Дарья Щербакова <daraaloec29@gmail.com>
2025-12-09 12:44:55 +0300

    creat

--- catalog/forms.py
+++ catalog/forms.py
@@ -26,6 +26,25 @@ class CategoryForm(forms.ModelForm):
             }
         )
 
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        description = cleaned_data.get('description')
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        if name in forbidden_words:
+            self.add_error('name', 'Поле "название" содержит запрещенное слово')
+
+            # Разделяем описание на слова
+        words_in_description = description.split()
+
+        for word in words_in_description:
+            # Приводим слово к нижнему регистру для проверки
+            normalized_word = word.lower().strip(",.!?;:()[]{}")  # Убираем знаки препинания
+            if normalized_word in forbidden_words:
+                self.add_error('description', 'Поле "описание" содержит запрещенное слово')
+                break  # Можно выйти из цикла после первой найденной ошибки
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -69,3 +88,28 @@ class ProductForm(forms.ModelForm):
                 'placeholder': 'Введите цену'
             }
         )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        description = cleaned_data.get('description')
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        if name in forbidden_words:
+            self.add_error('name', 'Поле "название" содержит запрещенное слово')
+
+            # Разделяем описание на слова
+        words_in_description = description.split()
+
+        for word in words_in_description:
+            # Приводим слово к нижнему регистру для проверки
+            normalized_word = word.lower().strip(",.!?;:()[]{}")  # Убираем знаки препинания
+            if normalized_word in forbidden_words:
+                self.add_error('description', 'Поле "описание" содержит запрещенное слово')
+                break  # Можно выйти из цикла после первой найденной ошибки
+
+    def clean_purchase_price(self):  # Исправлено имя метода на clean_purchase_price
+        purchase_price = self.cleaned_data.get('purchase_price')
+        if purchase_price is not None and purchase_price < 0:  # Проверяем на None
+            raise ValidationError('Цена не может быть отрицательной.')
+        return purchase_price  # Возвращаем очищенное значение
